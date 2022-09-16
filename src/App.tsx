693ed2ee71cb2c571ef13d6797fac1888649fae5
Julio Cesar Pedroso <juliocp@weg.net>
2022-09-16 18:01:45 -0300

    add main-image.png and style applied navbar and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
+    <>
+    <NavBar/>
     <Home/>
+    </>
 
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
-import NavBar from "../../components/NavBar"
+import MainImage from 'assets/images/main-image.png';
+import './styles.css';
 
 const Home = () => {
     return (
-        <>
-            <NavBar />
-            <h1>Home Page</h1>
-        </>
+        <div className="home-container">
+            <div className="home-card">
+                <div className="home-card-content">
+                    <div>
+                        <h1>O carro perfeito para você</h1>
+                        <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
+                    </div>
+                </div>
+                <div className="home-card-image">
+                    <img src={MainImage} alt="Imagem Principal" />
+                </div>
+
+            </div>
+            <div className="home-button-card">
+                <div>
+                    <button className="btn btn-primary">
+                        <h5>VER CATÁLOGO</h5>
+                    </button>
+                    <h4>Comece agora a navegar</h4>
+                </div>
+            </div>
+        </div>
     );
 
 }
