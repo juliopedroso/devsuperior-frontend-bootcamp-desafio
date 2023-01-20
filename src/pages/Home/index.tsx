f7b88378a5bd7d1e4c0018bf8ea88b3c2c70f0bb
Julio Cesar Pedroso <juliocp@weg.net>
2023-01-20 18:39:10 -0300

    fixed home page XL responsive

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,24 +4,26 @@ import './styles.css';
 const Home = () => {
     return (
         <div className="home-container">
-            <div className="home-card">
-                <div className="home-card-content">
-                    <div>
-                        <h1>O carro perfeito para você</h1>
-                        <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
+            <div className="home-container-content">
+                <div className="home-card">
+                    <div className="home-card-content">
+                        <div>
+                            <h1>O carro perfeito para você</h1>
+                            <p>Conheça nossos carros e dê mais um passo na realização do seu sonho</p>
+                        </div>
+                    </div>
+                    <div className="home-card-image">
+                        <img src={MainImage} alt="Imagem Principal" />
                     </div>
                 </div>
-                <div className="home-card-image">
-                    <img src={MainImage} alt="Imagem Principal" />
-                </div>
-            </div>
-            
-            <div className="home-button-card">
+
+                <div className="home-button-card">
                     <button className="btn">
                         <h6>VER CATÁLOGO</h6>
                     </button>
                     <h5>Comece agora a navegar</h5>
 
+                </div>
             </div>
         </div>
     );
