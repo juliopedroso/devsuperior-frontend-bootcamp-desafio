5ff77eacf34a0a71c193899c882575e2d1a80cb7
Julio Cesar Pedroso <juliocp@weg.net>
2023-01-23 07:10:09 -0300

    created catalog container content and added style breakpoints.

--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -7,48 +7,52 @@ const Catalog = () => {
 
 
         <div className="home-container">
-            <div className="container my-4">
-                <div className="search-bar-container">
+            <div className="home-container-content">
 
+                <div className="container my-4">
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="search-bar-container">
+                                <form className="form-inline">
+                                    <label>
+                                        <input type="text" name="name" placeholder="Digite sua busca" />
+                                    </label>
+                                    <input className="search-btn btn" type="submit" value="BUSCAR" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
-                    <form className="form-inline">
-                        <label>
-                            <input type="text" name="name" placeholder="Digite sua busca" />
-                        </label>
-                        <input className= "search-btn btn" type="submit" value="BUSCAR" />
-                    </form>
+                    <div className="row">
+                        <div className="col-sm-6 col-lg-4">
+                            <ProductCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4 ">
+                            <ProductCard />
+                        </div>
+
+                        <div className="col-sm-6 col-lg-4">
+                            <ProductCard />
+                        </div>
+
+                        <div className="col-sm-6 col-lg-4">
+                            <ProductCard />
+                        </div>
+
+                        <div className="col-sm-6 col-lg-4">
+                            <ProductCard />
+                        </div>
+
+                        <div className="col-sm-6 col-lg-4">
+                            <ProductCard />
+                        </div>
+                        <div className="col-sm-6 col-lg-4">
+                            <ProductCard />
+                        </div>
+                        
+                    </div>
 
                 </div>
-            </div>
-
-            <div className="container my-4">
-
-                <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard />
-                    </div>
-
-                </div>
-
             </div>
         </div>
     );
